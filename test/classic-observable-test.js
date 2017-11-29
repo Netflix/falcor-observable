1fc7aa4f27fe62363482313db41403a35ac7f88a
Laurence Rowe <lrowe@netflix.com>
2017-11-29 00:20:05 -0800

    chore: nest tests

--- test/classic-observable-test.js
+++ test/classic-observable-test.js
@@ -3,55 +3,85 @@
 const { Observable } = require("../src/classic-observable");
 const { expect } = require("chai");
 const { stub } = require("sinon");
+import type { IClassicObservable } from "../src/classic-observable";
 
-describe("Observable subscribe", function() {
-  it("functions", function(done) {
-    const onNext = stub();
-    Observable.of(1, 2, 3).subscribe(onNext, done, () => {
+describe("Classic Observable", function() {
+  describe("subscribe", function() {
+    it("functions", function(done) {
+      const onNext = stub();
+      Observable.of(1, 2, 3).subscribe(onNext, done, () => {
+        expect(onNext.args).to.deep.equal([[1], [2], [3]]);
+        done();
+      });
+    });
+
+    it("only onNext function", function() {
+      const onNext = stub();
+      Observable.of(1, 2, 3).subscribe(onNext);
       expect(onNext.args).to.deep.equal([[1], [2], [3]]);
-      done();
+    });
+
+    it("only onNext partial observer", function() {
+      const onNext = stub();
+      Observable.of(1, 2, 3).subscribe({ onNext });
+      expect(onNext.args).to.deep.equal([[1], [2], [3]]);
+    });
+
+    it("only onError partial observer", function() {
+      const onError = stub();
+      const err = new Error("Err");
+      Observable.throw(err).subscribe({ onError });
+      expect(onError.args).to.deep.equal([[err]]);
+    });
+
+    it("only onError function", function() {
+      const onError = stub();
+      const err = new Error("Err");
+      Observable.throw(err).subscribe(null, onError);
+      expect(onError.args).to.deep.equal([[err]]);
+    });
+
+    it("only onCompleted partial observer", function(done) {
+      Observable.of(1, 2, 3).subscribe({ onCompleted: done });
+    });
+
+    it("only onCompleted function", function(done) {
+      Observable.of(1, 2, 3).subscribe(null, null, done);
+    });
+
+    it("no arguments", function() {
+      Observable.of(1, 2, 3).subscribe();
+    });
+
+    it("empty partial observer", function() {
+      Observable.of(1, 2, 3).subscribe({});
     });
   });
 
-  it("only onNext function", function() {
-    const onNext = stub();
-    Observable.of(1, 2, 3).subscribe(onNext);
-    expect(onNext.args).to.deep.equal([[1], [2], [3]]);
-  });
-
-  it("only onNext partial observer", function() {
-    const onNext = stub();
-    Observable.of(1, 2, 3).subscribe({ onNext });
-    expect(onNext.args).to.deep.equal([[1], [2], [3]]);
-  });
-
-  it("only onError partial observer", function() {
-    const onError = stub();
-    const err = new Error("Err");
-    Observable.throw(err).subscribe({ onError });
-    expect(onError.args).to.deep.equal([[err]]);
-  });
-
-  it("only onError function", function() {
-    const onError = stub();
-    const err = new Error("Err");
-    Observable.throw(err).subscribe(null, onError);
-    expect(onError.args).to.deep.equal([[err]]);
-  });
-
-  it("only onCompleted partial observer", function(done) {
-    Observable.of(1, 2, 3).subscribe({ onCompleted: done });
-  });
-
-  it("only onCompleted function", function(done) {
-    Observable.of(1, 2, 3).subscribe(null, null, done);
-  });
-
-  it("no arguments", function() {
-    Observable.of(1, 2, 3).subscribe();
-  });
-
-  it("empty partial observer", function() {
-    Observable.of(1, 2, 3).subscribe({});
+  describe("from", function() {
+    it("adapts from classic observable", function() {
+      const disposable = {
+        isDisposed: false,
+        dispose() {
+          this.isDisposed = true;
+        }
+      };
+      let observer;
+      const classicObservable: IClassicObservable<number> = {
+        subscribe(obs, onError, onCompleted) {
+          observer = obs;
+          return disposable;
+        }
+      };
+      const obs = Observable.from(classicObservable);
+      const onNext = stub();
+      const onError = stub();
+      const onCompleted = stub();
+      const subscription = obs.subscribe({ onNext, onError, onCompleted });
+      (observer: any).onNext(1);
+      expect(onNext.args).to.deep.equal([[1]]);
+      subscription.dispose();
+      expect(disposable.isDisposed).to.equal(true);
+    });
   });
 });
--- test/es-observable-test.js
+++ test/es-observable-test.js
@@ -5,81 +5,83 @@ const { expect } = require("chai");
 const { stub } = require("sinon");
 import type { IClassicObservable } from "../src/classic-observable";
 
-describe("ES Observable subscribe", function() {
-  it("functions", function(done) {
-    const next = stub();
-    Observable.of(1, 2, 3).subscribe(next, done, () => {
+describe("ES Observable", function() {
+  describe("subscribe", function() {
+    it("functions", function(done) {
+      const next = stub();
+      Observable.of(1, 2, 3).subscribe(next, done, () => {
+        expect(next.args).to.deep.equal([[1], [2], [3]]);
+        done();
+      });
+    });
+
+    it("only next function", function() {
+      const next = stub();
+      Observable.of(1, 2, 3).subscribe(next);
       expect(next.args).to.deep.equal([[1], [2], [3]]);
-      done();
+    });
+
+    it("only next partial observer", function() {
+      const next = stub();
+      Observable.of(1, 2, 3).subscribe({ next });
+      expect(next.args).to.deep.equal([[1], [2], [3]]);
+    });
+
+    it("only error partial observer", function() {
+      const error = stub();
+      const err = new Error("Err");
+      Observable.throw(err).subscribe({ error });
+      expect(error.args).to.deep.equal([[err]]);
+    });
+
+    it("only error function", function() {
+      const error = stub();
+      const err = new Error("Err");
+      Observable.throw(err).subscribe(null, error);
+      expect(error.args).to.deep.equal([[err]]);
+    });
+
+    it("only complete partial observer", function(done) {
+      Observable.of(1, 2, 3).subscribe({ complete: done });
+    });
+
+    it("only complete function", function(done) {
+      Observable.of(1, 2, 3).subscribe(null, null, done);
+    });
+
+    it("no arguments", function() {
+      Observable.of(1, 2, 3).subscribe();
+    });
+
+    it("empty partial observer", function() {
+      Observable.of(1, 2, 3).subscribe({});
     });
   });
 
-  it("only next function", function() {
-    const next = stub();
-    Observable.of(1, 2, 3).subscribe(next);
-    expect(next.args).to.deep.equal([[1], [2], [3]]);
-  });
-
-  it("only next partial observer", function() {
-    const next = stub();
-    Observable.of(1, 2, 3).subscribe({ next });
-    expect(next.args).to.deep.equal([[1], [2], [3]]);
-  });
-
-  it("only error partial observer", function() {
-    const error = stub();
-    const err = new Error("Err");
-    Observable.throw(err).subscribe({ error });
-    expect(error.args).to.deep.equal([[err]]);
-  });
-
-  it("only error function", function() {
-    const error = stub();
-    const err = new Error("Err");
-    Observable.throw(err).subscribe(null, error);
-    expect(error.args).to.deep.equal([[err]]);
-  });
-
-  it("only complete partial observer", function(done) {
-    Observable.of(1, 2, 3).subscribe({ complete: done });
-  });
-
-  it("only complete function", function(done) {
-    Observable.of(1, 2, 3).subscribe(null, null, done);
-  });
-
-  it("no arguments", function() {
-    Observable.of(1, 2, 3).subscribe();
-  });
-
-  it("empty partial observer", function() {
-    Observable.of(1, 2, 3).subscribe({});
-  });
-});
-
-describe("ES Observable from", function() {
-  it("adapts from classic observable", function() {
-    const disposable = {
-      isDisposed: false,
-      dispose() {
-        this.isDisposed = true;
-      }
-    };
-    let observer;
-    const classicObservable: IClassicObservable<number> = {
-      subscribe(obs, onError, onCompleted) {
-        observer = obs;
-        return disposable;
-      }
-    };
-    const esObservable = Observable.from(classicObservable);
-    const next = stub();
-    const error = stub();
-    const complete = stub();
-    const subscription = esObservable.subscribe({ next, error, complete });
-    (observer: any).onNext(1);
-    expect(next.args).to.deep.equal([[1]]);
-    subscription.unsubscribe();
-    expect(disposable.isDisposed).to.equal(true);
+  describe("from", function() {
+    it("adapts from classic observable", function() {
+      const disposable = {
+        isDisposed: false,
+        dispose() {
+          this.isDisposed = true;
+        }
+      };
+      let observer;
+      const classicObservable: IClassicObservable<number> = {
+        subscribe(obs, onError, onCompleted) {
+          observer = obs;
+          return disposable;
+        }
+      };
+      const obs = Observable.from(classicObservable);
+      const next = stub();
+      const error = stub();
+      const complete = stub();
+      const subscription = obs.subscribe({ next, error, complete });
+      (observer: any).onNext(1);
+      expect(next.args).to.deep.equal([[1]]);
+      subscription.unsubscribe();
+      expect(disposable.isDisposed).to.equal(true);
+    });
   });
 });
