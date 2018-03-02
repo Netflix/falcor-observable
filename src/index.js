03e3c2acbdb7be659fc00a9ba418e70c33d02e6f
Laurence Rowe <lrowe@netflix.com>
2018-03-02 13:05:09 -0800

    feat: mergeMap, mergeAll, concatMap, concatAll

--- src/index.js
+++ src/index.js
@@ -12,3 +12,7 @@ export type {
 module.exports.Observable = require("./classic-observable").Observable;
 module.exports.map = require("./operators/map").map;
 module.exports.reduce = require("./operators/reduce").reduce;
+module.exports.concatMap = require("./operators/concatMap").concatMap;
+module.exports.concatAll = require("./operators/concatAll").concatAll;
+module.exports.mergeMap = require("./operators/mergeMap").mergeMap;
+module.exports.mergeAll = require("./operators/mergeAll").mergeAll;
